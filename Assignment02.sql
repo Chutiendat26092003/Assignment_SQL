8a4c1a0b8369658218c00da7141a6dd585db271c
ChuTienDat <91305197+Chutiendat26092003@users.noreply.github.com>
2022-01-02 16:37:10 +0700

    Add files via upload

--- Assignment02.sql
+++ Assignment02.sql
@@ -20,35 +20,41 @@ create table Product(
 	Descriptions nvarchar(200),
 	Unit nvarchar(30),
 	Price money,
-	QuantityAvailable int
+	QuantityAvailable int,
+	CompanyID int foreign key references ProductCompany(CompanyID),
+	TypeID int foreign key references ProductType(TypeID)
 )
 go
 
-create table ProductCompanyProduct(
-    CompanyID int foreign key references ProductCompany(CompanyID),
-	ProductID int foreign key references Product(ProductID)
+create table ProductType(
+    TypeID int primary key,
+	TypeName nvarchar(100),
+)
+go 
+
+create table ProductCompanyProductType(
+     
 )
 
 --3
-insert into ProductCompany values (123, N'Asus', N'USA', '983232'),
+insert into ProductCompany values (123, N'Asus', N'USA', '0983232'),
                                   (333, N'Apple', N'Việt Nam', '0932321111')
 
-insert into Product values (1000, N'Máy Tính T450', N'Máy nhập cũ',N'Chiếc', 1000, 10),
-                           (1001, N'Điện Thoại Nokia5670', N'Điện thoại đang hot',N'Chiếc', 200, 200),
-						   (1002, N'Máy In Samsung 450', N'Máy in đang loại bình',N'Chiếc', 100, 10),
-						   (1003, N'Iphone 13', N'Đẹp Meli',N'Chiếc', 2000, 100),
-						   (1004, N'Iphone 13 Promax', N'Nặng',N'Chiếc', 3000, 100)
+insert into ProductType values (50, N'Điện Thoại'),
+                                  (51, N'Máy Tính'),
+								  (52, N'Máy In')
 
-insert into ProductCompanyProduct values (123, 1000),
-                                         (123, 1001),
-										 (123, 1002),
-										 (333, 1003),
-										 (333, 1004)
+insert into Product values (1000, N'Máy Tính T450', N'Máy nhập cũ',N'Chiếc', 1000, 10, 123, 51),
+                           (1001, N'Điện Thoại Nokia5670', N'Điện thoại đang hot',N'Chiếc', 200, 200, 123, 50),
+						   (1002, N'Máy In Samsung 450', N'Máy in đang loại bình',N'Chiếc', 100, 10, 123, 52),
+						   (1003, N'Iphone 13', N'Đẹp Meli',N'Chiếc', 2000, 100, 333, 50),
+						   (1004, N'Iphone 13 Promax', N'Nặng',N'Chiếc', 3000, 100, 333, 50)
+
 
 --4
 select * from ProductCompany
 select * from Product
-select * from ProductCompanyProduct
+select * from ProductType
 
 --5
 --a
@@ -69,12 +75,37 @@ where QuantityAvailable < 11
 
 --e
 select CompanyName, ProductName from Product
-inner join ProductCompanyProduct on Product.ProductID = ProductCompanyProduct.ProductID
-inner join ProductCompany on ProductCompanyProduct.CompanyID = ProductCompany.CompanyID
+inner join ProductCompany on ProductCompany.CompanyID = Product.CompanyID
 where  CompanyName =  N'Asus' 
 
 --6
 --a Số hãng sản phẩm mà cửa hàng có
+select COUNT(CompanyID) AS 'CompanyNumber'
+from ProductCompany
+
+
+--b Số mặt hàng mà cửa hàng bán
+select COUNT(ProductID) AS 'ProductNumber'
+from Product
+
+--c Tổng số loại sản phẩm của mỗi hãng có trong cửa hàng
+select COUNT(TypeID) AS 'ProductNumber'
+from ProductType
+
+--d Tổng số đầu sản phẩm của toàn cửa hàng
+select COUNT(ProductID) AS 'ProductNumber'
+from Product
+
+--7
+--a
+alter table Product
+     add constraint ck_Pr check(Price > 0)
+
+--b
+alter table ProductCompany
+     add constraint ck_T check(left(Tel,1)= '0')
+
+
 
 
 
